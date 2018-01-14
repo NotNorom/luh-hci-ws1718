81a4dfec60dec1489da79199fcabaffbb462c630
Andre Julius <norom@jimileibniz.de>
2018-01-14 21:07:06 +0100

    Add popup toggle function

---
+++ app/js/popup.js
@@ -0,0 +1,22 @@
+var popupIsOpen = true;
+var popupContent = "";
+
+function togglePopup() {
+    console.log("toggling popup")
+    var elems = document.getElementsByClassName("popup-wrapper");
+    var fab = document.getElementsByClassName("fab");
+    if (popupIsOpen) {
+        for (var i = elems.length - 1; i >= 0; i--) {
+            elems[i].setAttribute("hidden", true);
+        }
+        fab[0].innerText = "+";
+        popupIsOpen = false;
+    } else {
+        for (var i = elems.length - 1; i >= 0; i--) {
+            elems[i].removeAttribute("hidden");
+        }
+        fab[0].innerText = "X";
+        popupIsOpen = true;
+    }
+    console.log(elems);
+}
