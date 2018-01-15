802ca5e295357ac019f95c2d5cc18e95e47db01a
Re2one <rbouarar@hotmail.de>
2018-01-15 13:43:26 +0100

    re2one started doing stuff

---
+++ app/js/initdb.js
@@ -0,0 +1,23 @@
+// nur kopiert, zeug passiert noch
+var popupIsOpen = false;
+var popupContent = "";
+
+function togglePopup() {
+    console.log("toggling popup")
+    var elems = document.getElementsByClassName("popup-wrapper");
+    var fab = document.getElementsByClassName("fab");
+    if (popupIsOpen) {
+        for (var i = elems.length - 1; i >= 0; i--) {
+            elems[i].setAttribute("hidden", true);
+        }
+        fab[0].innerText = "+";
+        popupIsOpen = false;
+    } else {
+        for (var i = elems.length - 1; i >= 0; i--) {
+            elems[i].removeAttribute("hidden");
+        }
+        fab[0].innerText = "x";
+        popupIsOpen = true;
+    }
+    console.log(elems);
+}
