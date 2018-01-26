99ebcf3ea70a02873c571c804914a8d5c683f0eb
Andre Julius <norom@jimileibniz.de>
2018-01-26 16:31:49 +0100

    Change to better colors. Add new icons to the comments upvote and downvote buttons

--- app/js/chart.js
+++ app/js/chart.js
@@ -5,22 +5,22 @@ function createChart() {
     chart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["leicht", "schwer", "langsam", "schnell", "interessant"],
+            labels: ["interessant", "leicht", "schwer", "langsam", "schnell"],
             datasets: [{
                 label: 'Anzahl Stimmen',
                 data: [0, 0, 0, 0, 0],
                 backgroundColor: [
                     'rgba(92, 107, 192, 0.6)',
-                    'rgba(121, 134, 203, 0.6)',
+                    'rgba(126,87,194 ,0.6)',
                     'rgba(239, 83, 80, 0.6)',
-                    'rgba(229, 115, 115, 0.6)',
+                    'rgba(255,112,67 ,0.6)',
                     'rgba(102, 187, 106, 0.6)'
                 ],
                 borderColor: [
                     'rgba(92, 107, 192, 1)',
-                    'rgba(121, 134, 203, 1)',
+                    'rgba(126,87,194 ,1)',
                     'rgba(239, 83, 80, 1)',
-                    'rgba(229, 115, 115, 1)',
+                    'rgba(255,112,67 ,1)',
                     'rgba(102, 187, 106, 1)'
                 ],
                 borderWidth: 1
